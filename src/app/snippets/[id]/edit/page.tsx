50a33f4a62c0c4bc37b3c526f1c555e43ddab6d7
Patrick Sheridan <patricksheridan@Patricks-MacBook-Pro.local>
2024-07-14 15:42:18 -0700

    Inital commit. Add, edit, view snippet added

---
+++ src/app/snippets/[id]/edit/page.tsx
@@ -0,0 +1,21 @@
+import { notFound } from 'next/navigation';
+import { db } from '@/db';
+
+interface SnippetEditPageProps {
+  params: {
+    id: string
+  }
+}
+
+export default async function SnippetEditPage(props: SnippetEditPageProps) {
+  const id = parseInt(props.params.id)
+  const snippet = await db.snippet.findFirst({
+    where: {id}
+  })
+
+  if (!snippet) {
+    return notFound();
+  }
+
+  return <div>Edit snippt with title {snippet.title}</div>
+};
---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,57 @@
+import { redirect } from 'next/navigation';
+import { db } from '@/db'
+
+export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    //Needs to be a server action
+    'use server';
+
+    //Check users input, validate
+    const title = formData.get('title') as string;
+    const code = formData.get('code') as string;
+
+    //Create a new record in db
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code
+      },
+    });
+    console.log(snippet)
+
+    //Redirect user to new code snippet page
+    redirect('/')
+  }
+
+  return ( 
+    <form action={createSnippet}>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="title">
+            Title
+          </label>
+          <input
+            name="title"
+            className="border rounder p-2 w-full"
+            id="title"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label className="w-12" htmlFor="code">
+            Code
+          </label>
+          <textarea
+            name="code"
+            className="border rounder p-2 w-full"
+            id="code"
+          />
+        </div>
+
+        <button type="submit" className="rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
+  )
+}
