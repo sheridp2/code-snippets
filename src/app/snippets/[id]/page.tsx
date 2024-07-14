ad0abecc176515b371b134856e16586e7c0b8ed7
Patrick Sheridan <patricksheridan@Patricks-MacBook-Pro.local>
2024-07-14 16:34:22 -0700

    Edit and delete added

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
 import { notFound } from 'next/navigation';
 import { db } from '@/db';
+import * as actions from '@/actions'
 
 interface renderedSnippets {
   params: {
-    id: string
+    id: string;
   }
 }
 
@@ -19,12 +20,16 @@ export default async function SnippetShowPage (props: renderedSnippets) {
     return notFound();
   }
 
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
+
   return <div>
     <div className='flex m-4 justify-between items-center'>
        <h1 className='text-xl font-bold'>{snippet.title}</h1>
        <div className='flex gap-4'>
         <Link href={`/snippets/${snippet.id}/edit`} className='p-2 border rounded'>Edit</Link>
-        <button className='p-2 border rounded'>Delete</button>
+        <form action={deleteSnippetAction}>
+          <button className='p-2 border rounded'>Delete</button>
+        </form>
        </div>
     </div>
     <pre className='p-3 border rounded bg-gray-200 border-gray-200'>
