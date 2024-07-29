da2b68bd32284334c22a219740d579fd1837b57f
Patrick Sheridan <patricksheridan@Patricks-MacBook-Pro.local>
2024-07-29 10:45:20 -0700

    Error handling added

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -19,3 +19,44 @@ export async function deleteSnippet(id: number) {
 
   redirect('/')
 }
+
+export async function createSnippet(formState: {message: string}, formData: FormData) {
+
+  try{
+    //Check users input, validate
+    const title = formData.get('title');
+    const code = formData.get('code');
+
+    if(typeof title !== 'string' || title.length < 3) {
+      return {
+        message: 'Title must be longer',
+      }
+    }
+    if(typeof code !== 'string' || code.length < 10) {
+      return {
+        message: 'Code must be longer',
+      }
+    }
+
+    //Create a new record in db
+    await db.snippet.create({
+      data: {
+        title,
+        code
+      },
+    });
+
+  } catch (err: unknown) {
+    if(err instanceof Error) {
+      return { 
+        message: err.message,
+      };
+    } else {
+      return {
+        message: 'Something went wrong...'
+      };
+    }
+  }
+  //Redirect user to new code snippet page
+  redirect('/')
+}
--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,30 +1,13 @@
-import { redirect } from 'next/navigation';
-import { db } from '@/db'
+'use client';
+
+import { useFormState } from "react-dom"
+import * as actions from '@/actions'
 
 export default function SnippetCreatePage() {
-  async function createSnippet(formData: FormData) {
-    //Needs to be a server action
-    'use server';
-
-    //Check users input, validate
-    const title = formData.get('title') as string;
-    const code = formData.get('code') as string;
-
-    //Create a new record in db
-    const snippet = await db.snippet.create({
-      data: {
-        title,
-        code
-      },
-    });
-    console.log(snippet)
-
-    //Redirect user to new code snippet page
-    redirect('/')
-  }
+  const [formState, action] = useFormState(actions.createSnippet, {message: ''});
 
   return ( 
-    <form action={createSnippet}>
+    <form action={action}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -47,7 +30,9 @@ export default function SnippetCreatePage() {
             id="code"
           />
         </div>
-
+        {
+          formState.message ? <div className="my-2 p-2 bg-red-200 border rounded border-red-400">{formState.message}</div> : null
+        }
         <button type="submit" className="rounded p-2 bg-blue-200">
           Create
         </button>
